46de3bb7820988197dea7c42bc9e8904a608c779
Erutase Akpobome <erutase.akpobome@liverpoolft.nhs.uk>
2021-08-01 15:14:36 +0100

    Add project files.

---
+++ TheCodeCamp/App_Start/WebApiConfig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Web.Http;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace TheCodeCamp
+{
+  public static class WebApiConfig
+  {
+    public static void Register(HttpConfiguration config)
+    {
+      // Web API configuration and services
+      AutofacConfig.Register();
+
+            config.AddApiVersioning(cfg =>
+            {
+                cfg.DefaultApiVersion = new ApiVersion(2, 0);
+                cfg.AssumeDefaultVersionWhenUnspecified = true;
+
+                cfg.ReportApiVersions =true;
+            });
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
+
+      // Web API routes
+      config.MapHttpAttributeRoutes();
+
+      //config.Routes.MapHttpRoute(
+      //    name: "DefaultApi",
+      //    routeTemplate: "api/{controller}/{id}",
+      //    defaults: new { id = RouteParameter.Optional }
+      //);
+    }
+  }
+}
